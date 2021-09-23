c106183fcce39a67aa0428c8b229b000838c2940
Lucaks-Steinhorst <57056885+Lucaks-Steinhorst@users.noreply.github.com>
2021-09-22 21:13:46 -0300

    Update Notas.js
    Refazendo a função mediaCA para poder mostrar corretamente a media final do aluno.

--- src/models/nota.js
+++ src/models/nota.js
@@ -15,7 +15,23 @@ class Nota {
     }
 
     mediaCA() {
-        return "SS";
+        const media =  Math.max(
+            0.4 * this.a1 + 0.6 * this.a2, 
+            0.4 * this.a1 + 0.6 * this.a3, 
+            0.4 * this.a3 + 0.6 * this.a2
+        )
+        if (media >= 0 && media < 1){
+            return "SR";
+        }else if (media >= 1 && media < 2.99 ){
+            return "II";
+        }else if (media >= 3 && media < 4.99 ){
+            return "MI";
+        }else if (media >= 5 && media < 6.99 ){
+            return "MM";
+        }else if (media >= 7 && media < 8.99 ){
+            return "MS";
+        }else {return "SS"}
+
     }
 }
 
