9c8447d928026133018badc685bc9b7ea3768182
Artur Leal <81257792+ArturLeaI@users.noreply.github.com>
2021-11-29 21:10:17 -0300

    Criação de teste unitários no /nota.test.js

--- test/unit/specs/nota.test.js
+++ test/unit/specs/nota.test.js
@@ -1,4 +1,32 @@
-const Nota = require('../src/models/nota');
+const Nota = require('./nota');
+
+describe('Menção da média final', () => {
+    test('SS caso a  nota for maior que 9', () => {
+        const nota = new Nota(null, 10, 10, null);
+        expect(nota.mediaCA()).toEqual("SS");
+    });
+    test(' MS caso a  nota for entre 7,0 e 8,9', () => {
+        const nota = new Nota(null, 7, 7, null);
+        expect(nota.mediaCA()).toEqual("MS");
+    });
+    test('MM caso a  nota for entre 5,0 e 6,9', () => {
+        const nota = new Nota(null, 6, 6, null);
+        expect(nota.mediaCA()).toEqual("MM");
+    });
+    test('MI caso a  nota for entre 3,0 e 4,9', () => {
+        const nota = new Nota(null, 5, 3, null);
+        expect(nota.mediaCA()).toEqual("MI");
+    });
+    test('II caso a  nota for entre 0,1 e 2,9', () => {
+        const nota = new Nota(null, 3, 2, null);
+        expect(nota.mediaCA()).toEqual("II");
+    });
+    test('SR caso a  nota for inferior a 0', () => {
+        const nota = new Nota(null, 0, 0, null);
+        expect(nota.mediaCA()).toEqual("SR");
+    });
+
+});
 
 describe('Calculo da média final', () => {
     test('a media é zero se não tem notas', () => {
@@ -8,7 +36,7 @@ describe('Calculo da média final', () => {
         expect(nota.mediaFinal()).toEqual(0);
     });
 
-    test('a média é (0.4 * a1) + (0.6 * a2) se tem apenas a1 e a2', () => {
+    test('a média é (0.3 * a1) + (0.6 * a2) se tem apenas a1 e a2', () => {
         let nota = new Nota(null, 3, 5, null);
         expect(nota.mediaFinal()).toEqual(0.4 * 3 + 0.6 * 5);
         nota = new Nota(null, 7, 4, null);
@@ -32,4 +60,3 @@ describe('Calculo da média final', () => {
         nota = new Nota(null, 6, 4, 7);
         expect(nota.mediaFinal()).toEqual(0.4 * 6 + 0.6 * 7);
     });
-});
